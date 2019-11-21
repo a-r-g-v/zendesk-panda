5df71573e2277a40078f6125c24b88b2f1514197
Yuki Mukasa <argvc0@gmail.com>
2019-11-21 21:03:30 +0900

    [RED] Add tests for get_unclosed_tickets

--- tests/test_service.py
+++ tests/test_service.py
@@ -1,5 +1,14 @@
+from typing import List
+
 from zendesk_panda.service import sent_into_slack
+from zendesk_panda.usecase import Ticket
 
 
 def test_sent_into_slack(config):
     sent_into_slack(config['slack_api_token'], config['slack_channel'], "test")
+
+
+def test_get_unclosed_tickets(config):
+    tickets: List[Ticket] = get_unclosed_tickets(config['zendesk_email'], config['zendesk_token'], config['zendesk_subdomain'])
+    assert len(tickets) == 1
+    assert tickets[0].id == 1
