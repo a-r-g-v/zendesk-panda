f987707fb286b4e6c5c8db492a173fcfb9727ebe
Yuki Mukasa <argvc0@gmail.com>
2019-11-21 19:47:35 +0900

    Format message

--- tests/test_usecase.py
+++ tests/test_usecase.py
@@ -2,9 +2,8 @@ from zendesk_panda.usecase import generate_remind_message
 
 
 def test_generate_remind_message():
-    expected = """Waiting for response
-    [#1] 見積もり依頼 (a-r-g-v, argvc0@gmail.com)
-    2 days stale · 2 days old · Open · Waiting on @agent1, @agent2, @agent3
-    """
+    expected = ( "Waiting for response\n" +
+    "[#1] 見積もり依頼 (a-r-g-v, argvc0@gmail.com)\n" +
+    "2 days stale · 2 days old · Open · Waiting on @agent1, @agent2, @agent3\n" )
 
     assert generate_remind_message() == expected
--- zendesk_panda/usecase.py
+++ zendesk_panda/usecase.py
@@ -1,6 +1,9 @@
 
 def generate_remind_message() -> str:
-    return """Waiting for response
-    [#1] 見積もり依頼 (a-r-g-v, argvc0@gmail.com)
-    2 days stale · 2 days old · Open · Waiting on @agent1, @agent2, @agent3
-    """
+    header = "Waiting for response\n"
+
+    ticket = ("[#1] 見積もり依頼 (a-r-g-v, argvc0@gmail.com)\n" +
+    "2 days stale · 2 days old · Open · Waiting on @agent1, @agent2, @agent3")
+
+
+    return header + ticket + "\n"
