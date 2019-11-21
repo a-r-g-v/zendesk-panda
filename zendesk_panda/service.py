8a8ae62ee7d2610936042d5f235b8daacd2e583d
Yuki Mukasa <argvc0@gmail.com>
2019-11-21 21:00:55 +0900

    Add type hint

--- zendesk_panda/service.py
+++ zendesk_panda/service.py
@@ -1,5 +1,5 @@
 from slacker import Slacker
 
 
-def sent_into_slack(slack_api_token, channel, message) -> None:
+def sent_into_slack(slack_api_token: str, channel: str, message: str) -> None:
     Slacker(slack_api_token).chat.post_message(channel, message)
