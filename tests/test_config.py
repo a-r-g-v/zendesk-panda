bef21fbfc063639fed7dd5d637e8f4eaec08119c
Yuki Mukasa <argvc0@gmail.com>
2019-11-21 18:42:27 +0900

    Add load_config success case

--- tests/test_config.py
+++ tests/test_config.py
@@ -6,3 +6,17 @@ from zendesk_panda.config import load_config
 def test_load_config_raises_key_error():
     with pytest.raises(KeyError):
         load_config(source={})
+
+
+def test_load_config():
+    source = dict(
+        ZENDESK_EMAIL = "ZENDESK_EMAIL",
+        ZENDESK_TOKEN = "ZENDESK_TOKEN",
+        ZENDESK_SUBDOMAIN = "ZENDESK_SUBDOMAIN",
+
+    )
+    config = load_config(source)
+
+    assert config["email"] == "ZENDESK_EMAIL"
+    assert config["token"] == "ZENDESK_TOKEN"
+    assert config["subdomain"] == "ZENDESK_SUBDOMAIN"
