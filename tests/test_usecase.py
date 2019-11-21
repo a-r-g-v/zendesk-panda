6e456deea1d993219219a7a2d3d428bf854f0b00
Yuki Mukasa <argvc0@gmail.com>
2019-11-21 19:40:14 +0900

    Add status in remind message

--- tests/test_usecase.py
+++ tests/test_usecase.py
@@ -4,7 +4,7 @@ from zendesk_panda.usecase import generate_remind_message
 def test_generate_remind_message():
     expected = """Waiting for response
     [#1] 見積もり依頼 (a-r-g-v, argvc0@gmail.com)
-    2 days stale · 2 days old · Waiting on @agent1, @agent2, @agent3
+    2 days stale · 2 days old · Open · Waiting on @agent1, @agent2, @agent3
     """
 
     assert generate_remind_message() == expected
--- zendesk_panda/usecase.py
+++ zendesk_panda/usecase.py
@@ -2,5 +2,5 @@
 def generate_remind_message() -> str:
     return """Waiting for response
     [#1] 見積もり依頼 (a-r-g-v, argvc0@gmail.com)
-    2 days stale · 2 days old · Waiting on @agent1, @agent2, @agent3
+    2 days stale · 2 days old · Open · Waiting on @agent1, @agent2, @agent3
     """
