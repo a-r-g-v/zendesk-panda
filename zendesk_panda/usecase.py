36a3930eccdb8ad48d547ac3613de8aa2e859510
Yuki Mukasa <argvc0@gmail.com>
2019-11-21 20:16:11 +0900

    Fix code style

--- zendesk_panda/usecase.py
+++ zendesk_panda/usecase.py
@@ -20,5 +20,5 @@ def generate_remind_message(tickets: List[Ticket]) -> str:
         message += ("[#{id}] {title} ({requester_slack}, {requester})\n".format(**ticket.__dict__) +
                     "{staled} stale · {created} old · {status} · Waiting on {waiting_on}".format(**ticket.__dict__))
 
-
     return header + message + "\n"
+
