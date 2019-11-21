8be8e9abfbecce547d90a1d867368b1bb1e8e554
Yuki Mukasa <argvc0@gmail.com>
2019-11-21 18:39:41 +0900

    Refactored

--- tests/test_config.py
+++ tests/test_config.py
@@ -3,6 +3,6 @@ import pytest
 from zendesk_panda.config import load_config
 
 
-def test_load_config():
+def test_load_config_raises_key_error():
     with pytest.raises(KeyError):
         load_config(source={})
