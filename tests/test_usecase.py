3dbae4f5db3d34ded6c18bae4d281a2a1221c214
Yuki Mukasa <argvc0@gmail.com>
2019-11-21 18:47:10 +0900

    [RED] Add testcase of generate_remind_message

---
+++ tests/test_usecase.py
@@ -0,0 +1,8 @@
+
+def test_generate_remind_message():
+    expected = """Waiting for response
+    [#1] 見積もり依頼 (a-r-g-v, argvc0@gmail.com)
+    2 days stale · 2 days old · Waiting on @agent1, @agent2, @agent3
+    """
+
+    assert generate_remind_message() == expected
