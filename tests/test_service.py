de33b27504d42586ccc57f6e0e2a42d76e82c100
Yuki Mukasa <argvc0@gmail.com>
2019-11-21 21:40:27 +0900

    [GREEN] Add get_unclosed_tickets

--- tests/test_service.py
+++ tests/test_service.py
@@ -10,4 +10,4 @@ def test_sent_into_slack(config):
 
 def test_get_unclosed_tickets(config):
     tickets: List[Ticket] = get_unclosed_tickets(config['zendesk_email'], config['zendesk_token'], config['zendesk_subdomain'])
-    assert len(tickets) == 1
+    assert len(list(tickets)) >= 1
--- zendesk_panda/service.py
+++ zendesk_panda/service.py
@@ -1,6 +1,8 @@
-from typing import List
+from typing import Iterable
 
 from slacker import Slacker
+from zenpy import Zenpy
+from zenpy.lib.api_objects import Ticket as ZendeskTicket, User as ZendeskUser
 
 from zendesk_panda.usecase import Ticket
 
@@ -9,5 +11,11 @@ def sent_into_slack(slack_api_token: str, channel: str, message: str) -> None:
     Slacker(slack_api_token).chat.post_message(channel, message)
 
 
-def get_unclosed_tickets(email: str, token: str, subdomain: str) -> List[Ticket]:
-    return [Ticket(1, "見積もり依頼", "argvc0@gmail.com", "a-r-g-v", "Open", "@agent1, @agent2, @agent3", None, None)]
+def get_unclosed_tickets(email: str, token: str, subdomain: str) -> Iterable[Ticket]:
+    zenpy = Zenpy(subdomain, email, token)
+    ticket: ZendeskTicket
+    for ticket in zenpy.tickets():
+        requester: ZendeskUser = ticket.requester
+        waiting_on = None
+        staled_at = None
+        yield Ticket(ticket.id, ticket.subject, requester.email, requester.email, ticket.status, waiting_on, staled_at, ticket.created_at)
